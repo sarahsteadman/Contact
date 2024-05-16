d12242e11c384f27fb4ce5470ced54b27c656709
sarahsteadman <fairyfaye4real@gmail.com>
2024-05-15 19:24:49 -0600

    change access control

--- app.js
+++ app.js
@@ -14,8 +14,17 @@ app.use(bodyParser.json());
 
 // Set up CORS headers
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-with, Content-Type, Accept, Z-Key",
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS",
+    );
+    next(); // Call next middleware or route handler
 });
 
 // Mount routes
